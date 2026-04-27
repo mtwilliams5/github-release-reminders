1167489d668f645d97ae0fc91088348114f0be8e
Matt Williams <matt.williams@playerslounge.com>
2026-04-27 09:32:05 +0100

    fix ts and eslint warnings

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,7 +6,7 @@ module.exports = {
     project: ['./tsconfig.json', './tsconfig.test.json'],
     tsconfigRootDir: __dirname,
   },
-  ignorePatterns: ['.eslintrc.js', 'scripts/**/*.js'],
+  ignorePatterns: ['.eslintrc.js', 'jest.config.js', 'scripts/**/*.js'],
   plugins: ['@typescript-eslint', 'import'],
   extends: [
     'airbnb-base',
